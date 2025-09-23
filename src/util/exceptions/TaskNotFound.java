e36da56ff78632bb2a1682522bc71f1345e57277
Crodi <Crodis@yandex.ru>
2025-09-23 21:24:26 +0300

    feat: exception TaskNotFound

---
+++ src/util/exceptions/TaskNotFound.java
@@ -0,0 +1,16 @@
+package util.exceptions;
+
+public class TaskNotFound extends RuntimeException {
+
+    public TaskNotFound() {
+        super();
+    }
+
+    public TaskNotFound(String message) {
+        super(message);
+    }
+
+    public TaskNotFound(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
